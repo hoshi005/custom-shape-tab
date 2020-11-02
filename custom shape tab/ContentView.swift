51191bcd480f9621bdbfa884ea262f362aa3fa39
Susumu Hoshikawa <hoshi005@me.com>
2020-11-02 17:47:19 +0900

    first commit

---
+++ custom shape tab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  custom shape tab
+//
+//  Created by Susumu Hoshikawa on 2020/11/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ custom shape tab/custom_shape_tabApp.swift
@@ -0,0 +1,17 @@
+//
+//  custom_shape_tabApp.swift
+//  custom shape tab
+//
+//  Created by Susumu Hoshikawa on 2020/11/02.
+//
+
+import SwiftUI
+
+@main
+struct custom_shape_tabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
